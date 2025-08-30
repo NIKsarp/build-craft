b09515d31c2379ed96f6943100126a7a8c77a285
Prashant Rastogi <publicidforpeople>
2025-08-30 20:34:17 +0530

    fix anchor link in telephone number and email

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -202,7 +202,9 @@ export const Footer = () => {
                     <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
                   </svg>
                 </div>
-                <span className="text-gray-300">+91 98765 43210</span>
+                <a href="tel:9876543210" className="text-gray-300">
+                  +91 98765 43210
+                </a>
               </div>
               <div className="flex items-center space-x-3">
                 <div className="h-5 w-5 text-orange-400">
@@ -221,7 +223,9 @@ export const Footer = () => {
                     <polyline points="22,6 12,13 2,6" />
                   </svg>
                 </div>
-                <span className="text-gray-300">info@buildcraft.com</span>
+                <a href="mailto:info@buildcraft.com" className="text-gray-300">
+                  info@buildcraft.com
+                </a>
               </div>
               <div className="flex items-start space-x-3">
                 <div className="h-5 w-5 text-orange-400 mt-1">
