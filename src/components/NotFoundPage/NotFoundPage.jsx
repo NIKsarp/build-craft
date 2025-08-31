a89dc358bc95b6761e2b6ceba409c84c2a7782d4
Prashant Rastogi <publicidforpeople>
2025-08-31 13:44:40 +0530

    fix path tag and make better ui for NotFoundPage.jsx

--- src/components/NotFoundPage/NotFoundPage.jsx
+++ src/components/NotFoundPage/NotFoundPage.jsx
@@ -3,13 +3,13 @@ import { NavLink } from "react-router-dom";
 export const NotFoundPage = () => {
   return (
     <div
-      className={`dark:bg-black   bg-gradient-to-br from-slate-900 via-blue-900 to-indigo-900  text-white flex flex-col text-center py-18 gap-8`}
+      className={`dark:bg-black   bg-gradient-to-br from-slate-900 via-blue-900 to-indigo-900  text-white flex flex-col text-center py-18 gap-8 justify-center items-center`}
     >
       <h2 className="text-5xl md:text-9xl">404 - Page Not Found</h2>
       <p>The page you are looking for does not exist.</p>
       <NavLink
         to="/"
-        className="rounded-full border-opacity-0  p-4 font-bold transition-all duration-300 ease-in hover:bg-white border-4 hover:text-black  hover:border-blue-800"
+        className="rounded-full border-opacity-0  p-4 font-bold transition-all duration-300 ease-in hover:bg-white border-4 hover:text-black w-96   hover:border-blue-800"
       >
         Go to Homepage
       </NavLink>
